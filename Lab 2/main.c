24356979bf16ef61b4611ce94da42c984e35598a
NotCosyMax <max.wennerfeldt@gmail.com>
2016-09-08 10:21:32 +0200

    Added labs

---
+++ Lab 2/main.c
@@ -0,0 +1,38 @@
+/*main file for Lab 2 in TDTS06
+Written by :
+Oskar von Heideken, oskvo980@student.liu.se
+and
+Max Wennerfeldt, maxwe992@student.liu.se
+
+The goal of the lab is to create a Proxy!
+*/
+
+/*
+Server part - receives the HTTP req. from the browser and delivers it to the client part
+Client part - determines to which web server it should connect and sends the HTTP req. and receives the HTTP response.
+              The Client then delivers the received content to the server part which sends it to the browser
+
+*/
+
+/*
+Requirement specifications:
+-The proxy should support both HTTP/1.0 and HTTP/1.1.
+-Handles simple HTTP GET interactions between client and server
+-Blocks requests for undesirable URLs, using HTTP redirection to display this error page instead
+-Detects inappropriate content bytes within a Web page before it is returned to the user, and redirecting to this error page
+-Imposes no limit on the size of the transferred HTTP data
+    Note: Using the realloc() function to increase the size of the buffer (allocated to receiving the HTTP data from the server) is also considered as imposing a limit. Instead, you must chose a size for the buffer and manage the data in it intelligently so that it does not result in lost packets or unsent data being overwritten when receiving responses. It is also recommended that you avoid performing type casting operations when receiving and sending data from the buffer.
+-Is compatible with all major browsers (e.g. Internet Explorer, Mozilla Firefox, Google Chrome, etc.) without the requirement to tweak any advanced feature
+-Allows the user to select the proxy port (i.e. the port number should not be hard coded)
+-Is smart in selection of what HTTP content should be searched for the forbidden keywords. For example, you probably agree that it is not wise to search inside compressed or other non-text-based HTTP content such as graphic files, etc.
+-(Optional) Supporting file upload using the POST method
+-You do not have to relay HTTPS requests through the proxy
+*/
+
+int main(void)
+{
+
+
+
+
+}
